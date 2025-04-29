7969485af5aa03d6102e0d9a7bc6dddc382328e1
Bogdan <bogdanatrosenko@gmail.com>
2025-04-29 21:08:51 +0300

    Made task

--- src/Inheritance.ts
+++ src/Inheritance.ts
@@ -1,27 +1,53 @@
-interface IPersonData {
+export interface IPersonData {
     name: string;
     secondName: string;
     age: number;
 }
 
-interface IStudentData extends IPersonData {
-    phone: string;
+export interface IStudentData extends IPersonData {
+    phone?: string;
 }
 
 /*
-1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IPersonData
-2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IPersonData
- */
+1. Напишите конструкторы для классов Person и Student, которые принимают в качестве параметра объекты с интерфейсами IPersonData и IStudentData соответственно.
+*/
+
 export class Person {
     readonly _name: string;
     readonly _secondName: string;
     readonly _age: number;
+
+    // Конструктор класса Person, принимает объект с типом IPersonData
+    constructor(personData: IPersonData) {
+        this._name = personData.name;
+        this._secondName = personData.secondName;
+        this._age = personData.age;
+    }
+
+    // Метод для получения данных о человеке
+    getData(): IPersonData {
+        return {
+            name: this._name,
+            secondName: this._secondName,
+            age: this._age,
+        };
+    }
 }
 
-/*
-1. Напишите конструктор, который принимает в качестве параметра объект с интерфейсом IStudentData
-2. Добавьте метод getData(), возвращающий объект, соответствующий интерфейсу IStudentData
- */
 export class Student extends Person {
     readonly _phone: string;
+
+    // Конструктор класса Student, принимает объект с типом IStudentData
+    constructor(studentData: IStudentData) {
+        super(studentData);  // Вызываем конструктор родительского класса Person
+        this._phone = studentData.phone || ''; // phone может быть необязательным
+    }
+
+    // Метод для получения данных о студенте
+    getData(): IStudentData {
+        return {
+            ...super.getData(), // Получаем данные из родительского класса
+            phone: this._phone,  // Добавляем поле phone, которое есть только у студентов
+        };
+    }
 }
